577fc5fc9c6b9463946b105d95cbb0b45f8957dd
idk <hankhill19580@gmail.com>
2022-10-05 15:14:30 -0400

    put keys in docker volumes

--- docker.sh
+++ docker.sh
@@ -2,4 +2,9 @@
 
 docker build -t rtchat .
 docker rm -f rtchat
-docker run ${docker_opts} -it --net=host --name=rtchat rtchat
+docker run ${docker_opts} -it \
+    --volume=$(pwd)/i2pkeys:/i2pkeys \
+    --volume=$(pwd)/onionkeys:/onionkeys \
+    --volume=$(pwd)/tlskeys:/tlskeys \
+    --net=host --name=rtchat rtchat
+docker logs -f rtchat
